a4ef010b4d10aa07006da49f8a668b38a61b2b4f
Vishu2706 <vatsalmevada2707@gmail.com>
2024-12-29 21:10:12 +0000

    new file added

---
+++ com/collection/Emp.java
@@ -0,0 +1,27 @@
+package com.collection;
+
+public class Emp {
+	private int eId;
+	private String eName;
+	private double eSalary;
+	
+	public int geteId() {
+		return eId;
+	}
+	public void seteId(int eId) {
+		this.eId = eId;
+	}
+	public String geteName() {
+		return eName;
+	}
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+	public double geteSalary() {
+		return eSalary;
+	}
+	public void seteSalary(double salary) {
+		this.eSalary = salary;
+	}
+	
+}
